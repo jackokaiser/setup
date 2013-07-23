f32c325cb39518e6e03bf66a77018ed3df80c53c
jackokaiser <jacko.kaiser@gmail.com>
2013-07-23 20:46:59 +0200

    my own version of setup

--- setup.sh
+++ setup.sh
@@ -39,10 +39,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
-ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+git clone https://github.com/jackokaiser/myDotfiles.git
 
+myDotfiles/install.sh
+sudo chsh -s /bin/zsh
